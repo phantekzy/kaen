5ebd8f6d1710ddcc82c2dd708b51f50e10a5d674
phantekzy <mainilotfi@gmail.com>
2025-12-14 23:13:09 +0100

    supabase configuration

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -25,7 +25,6 @@ export const Navbar = () => {
         <nav className="fixed top-0 w-full z-40 bg-[rgba(10,10,10,0.8)] backdrop-blur-lg border-b border-white/10 shadow-lg">
             <div className="max-w-5xl mx-auto px-4">
                 <div className="flex justify-between items-center h-16">
-                    {/* Logo Link (Keep this as is) */}
                     <Link
                         className="flex flex-row items-center justify-center font-mono text-xl font-bold text-white"
                         to={"/"}
---
+++ src/supabase-client.ts
@@ -0,0 +1,6 @@
+/* Import section */
+import { createClient } from "@supabase/supabase-js";
+/* Supabase URL */
+const supabaseURL = "https://qsbmkwuynlcjcpivjaxt.supabase.co";
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+export const supabase = createClient(supabaseURL, supabaseAnonKey);
