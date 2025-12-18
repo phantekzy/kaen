a3cc4ef1d33e73bde574f4d3f40c9e98cf757f5e
Maini Lotfi Abdelkader <mainilotfi@gmail.com>
2025-12-19 00:31:22 +0100

    changed the padding and the spacing it was a mess

--- src/pages/CreatePostPage.tsx
+++ src/pages/CreatePostPage.tsx
@@ -3,8 +3,8 @@ import { CreatePost } from "../components/CreatePost";
 /* Create post page */
 export const CreatePostPage = () => {
   return (
-    <div className="bg-black min-h-screen pt-20 sm:pt-32 pb-10">
-      <div className="flex flex-col items-center mb-10 space-y-1 sm:space-y-2">
+    <div className="bg-black min-h-screen pt-3 ">
+      <div className="flex flex-col items-center space-y-1 sm:space-y-2">
         <h1 className="text-5xl sm:text-7xl md:text-8xl font-extrabold text-center text-white tracking-tight">
           Draft your
         </h1>
